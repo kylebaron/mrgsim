d5e8d8109525e02b958126a89a0e111b99b75b6e
Kyle Baron <kyleb@metrumrg.com>
2017-07-03 15:19:29 -0500

    clean up

---
+++ Makefile
@@ -0,0 +1,49 @@
+SHELL := /bin/bash
+LIBDIR=/Users/kyleb/Rlibs/lib
+PACKAGE=mrgscript
+VERSION=$(shell grep Version DESCRIPTION |awk '{print $$2}')
+TARBALL=${PACKAGE}_${VERSION}.tar.gz
+PKGDIR=.
+CHKDIR=.
+
+## Set libPaths:
+export R_LIBS=${LIBDIR}
+
+ec:
+	echo ${VERSION}
+
+cran:
+	make doc
+	make build
+	R CMD CHECK --as-cran ${TARBALL} -o ${CHKDIR}
+
+all:
+	make doc
+	make build
+	make install
+
+.PHONY: doc
+doc:
+	Rscript -e 'library(devtools); document()'
+
+build:
+	R CMD build --md5 $(PKGDIR)
+
+
+install:
+	R CMD INSTALL --install-tests ${TARBALL}
+
+install-build:
+	R CMD INSTALL --build --install-tests ${TARBALL}
+
+check:
+	make doc
+	make build
+	R CMD CHECK ${TARBALL} -o ${CHKDIR}
+
+push:
+	make all
+	git commit -am "make push"
+	git push -u origin master
+
+
--- R/mrgscript.R
+++ R/mrgscript.R
@@ -1,8 +1,22 @@
 
-##' @export
-handle_period <- function(x) do.call("ev", x)
 
+##' Accessor functions.
+##'
+##' @param x sim object
+##'
+##' @rdname access
 ##' @export
+arms <- function(x) return(x$arms)
+
+##' @rdname access
+##' @export
+sequences <- function(x) return(x$sequences)
+
+##' @rdname access
+##' @export
+covsets <- function(x) return(x$covsets)
+
+# handle all periods in the runobject
 handle_periods <- function(x) {
   if(is.na(x$sequence[1])) {
     x$sequence <- names(x$period)
@@ -15,7 +29,9 @@ handle_periods <- function(x) {
   x
 }
 
-##' @export
+# handle a single period
+handle_period <- function(x) do.call("ev", x)
+
 then_ev <- function(df,x) {
   if(nrow(df) <=1) return(df)
   tim <- sapply(x$period,FUN = function(y) exists("time", y))
@@ -27,11 +43,6 @@ then_ev <- function(df,x) {
   df
 }
 
-
-# Take in the workflow object and return events
-# named by sequence name
-
-##' @export
 make_periods <- function(x) {
   ev <- vector(mode="list", length=length(x$sequence))
   names(ev) <- names(x$sequence)
@@ -45,7 +56,6 @@ make_periods <- function(x) {
   lapply(ev,as.ev)
 }
 
-##' @export
 assemble_formula <- function(...,distribution,call=NULL,formula=NULL,
                              lb=NULL,ub=NULL,by=NULL,as=NULL,var) {
   if(!is.null(formula)) return(formula)
@@ -65,13 +75,11 @@ assemble_formula <- function(...,distribution,call=NULL,formula=NULL,
   paste0(as,"~",call)
 }
 
-##' @export
 make_cov_formulae <- function(x) {
   y <- add_name_list(x$covariate,where="var")
   lapply(y,FUN=do.call,what=assemble_formula)
 }
 
-##' @export
 make_covsets <- function(x,cov_form=NULL) {
   if(is.null(cov_form)) cov_form <- make_cov_formulae(x)
   lapply(x$covset, function(y) {
@@ -83,47 +91,73 @@ make_designs <- function(x) {
   lapply(x, do.call, what=mrgsolve::tgrid)
 }
 
-
-##' @export
-get_subjects <- function(x) {
-
-  x$arm <- add_name_list(x$arm, where="arm")
-  ans <- lapply(x$arm, function(y) {
-    data_frame(arm=y$arm,ID=seq_len(y$nid),
-               sequence=y$sequence,covset=y$covset,sample=y$sample)
-  })
-  ans <- bind_rows(ans)
-  ans <- dplyr::mutate(ans,
-                       .covsetn = match(covset,names(x$covset)),
-                       .armn  = match(arm,names(x$arm)),
-                       .sequencen = match(sequence,names(x$sequence)),
-                       .sample = match(sample,names(x$sample)),
-                       ID=seq_len(n()))
-  dplyr::select(ans,ID,.armn,.covsetn,.sequencen)
-}
-
-
-used_sequences <- function(x) {
-  sapply(x$arm,function(y) y$sequence)
-}
-
-
-do_covariates <- function(x) length(x$covset) > 0
-
-##' @export
 get_arms <- function(x) {
   ans <- bind_rows(lapply(x$arm, dplyr::as_data_frame))
   ans$arm <- names(x$arm)
   return(ans)
 }
 
-##' @export
 get_sequences <- function(x) {
   sequence <- names(x$sequence)
   periods <- sapply(x$sequence,FUN=paste, collapse=",")
   data_frame(sequence=sequence,periods=periods)
 }
 
+calculate_ids <- function(nid) {
+  end <- cumsum(nid)
+  start <- c(0,end[-length(end)])+1
+  ans <- mapply(start,end,SIMPLIFY=FALSE,FUN=function(x,y) {
+    seq(x,y)
+  })
+  names(ans) <- names(x$arm)
+  ans
+}
+
+get_ids <- function(x) {
+ nid <- s_pick(x$arm,"nid")
+ calculate_ids(nid)
+}
+
+##' Simulate from a scripted run.
+##'
+##' @param mod a model object
+##' @param x the simrun object
+##' @param .Request requested endpoints
+##' @details
+##' The object passed as \code{x} is the
+##' result of a call to \code{\link{load_run}}.
+##'
+##' @export
+sim_run <- function(mod,x,.Request=x$endpoints) {
+  out <- vector(mode="list", length(length(x$arm)))
+  ids <- get_ids(x)
+  for(i in seq_along(x$arm)) {
+    .arm <- slice(x$arms,i)
+    ev <- x$periods[[.arm$sequence]]
+    this_arm <- slice(x$arms,rep(i,.arm$nid))
+    this_arm$ID <- ids[[i]]
+    des <- x$designs[[.arm$sample]]
+    idata <- dplyr::select(this_arm,-sample,-sequence,-covset,-arm)
+    idata <- mutate_random(idata,x$covsets[[.arm$covsetn]],x$envir)
+    out[[i]] <- mrgsim(mod,
+                       idata=idata,
+                       events=ev,
+                       tgrid=des,
+                       obsonly=TRUE,
+                       Req=.Request,
+                       carry.out="armn")
+    out[[i]] <- as_data_frame(out[[i]])
+    out[[i]] <- mutate(out[[i]],arm=.arm$arm)
+    out[[i]] <- dplyr::select(out[[i]],ID,time,arm,armn,everything())
+  }
+  bind_rows(out)
+}
+
+
+##' Load a simulation run from file.
+##'
+##' @param file the name of a file containing \code{yaml} run specification
+##'
 ##' @export
 load_run <- function(file) {
 
@@ -178,9 +212,9 @@ load_run <- function(file) {
   x$sequences <- get_sequences(x)
 
   periods <- make_periods(x)
-  idata <- get_subjects(x)
 
   covsets <- make_covsets(x)
+
   check_covsets(covsets,x)
 
   x$designs <- make_designs(x$sample)
@@ -193,58 +227,3 @@ load_run <- function(file) {
   x
 }
 
-##' @export
-sim_run <- function(mod,x,join=FALSE,.Request=x$endpoints) {
-  out <- vector(mode="list", length(length(x$arm)))
-  ids <- get_ids(x)
-  for(i in seq_along(x$arm)) {
-    .arm <- slice(x$arms,i)
-    ev <- x$periods[[.arm$sequence]]
-    this_arm <- slice(x$arms,rep(i,.arm$nid))
-    this_arm$ID <- ids[[i]]
-    des <- x$designs[[.arm$sample]]
-    idata <- dplyr::select(this_arm,-sample,-sequence,-covset,-arm)
-    idata <- mutate_random(idata,x$covsets[[.arm$covsetn]],x$envir)
-    out[[i]] <- mrgsim(mod,
-                       idata=idata,
-                       events=ev,
-                       tgrid=des,
-                       obsonly=TRUE,
-                       Req=.Request,
-                       carry.out="armn")
-    out[[i]] <- as_data_frame(out[[i]])
-    out[[i]] <- mutate(out[[i]],arm=.arm$arm)
-    out[[i]] <- dplyr::select(out[[i]],ID,time,arm,armn,everything())
-  }
-  bind_rows(out)
-}
-
-
-calculate_ids <- function(nid) {
-  end <- cumsum(nid)
-  start <- c(0,end[-length(end)])+1
-  ans <- mapply(start,end,SIMPLIFY=FALSE,FUN=function(x,y) {
-    seq(x,y)
-  })
-  names(ans) <- names(x$arm)
-  ans
-}
-
-get_ids <- function(x) {
- nid <- s_pick(x$arm,"nid")
- calculate_ids(nid)
-}
-
-##' @export
-get_idata <- function(x) {
-  a <- get_subjects(x)
-  a <- group_by(a,.covsetn)
-  ungroup(do(a,apply_covset(.,x)))
-}
-
-
-apply_covset <- function(df,x) {
-  n <- as.integer(df[1,".covsetn"])
-  covset <- x$covsets[[n]]
-  mutate_random(df,covset,envir=x$envir)
-}
